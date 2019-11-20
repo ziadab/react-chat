9d2d64f883a80c1a73db68e89307c93a4ae64901
Ziad Abouelfarah <zain.fake02@gmail.com>
2019-11-20 02:14:22 +0100

    socket.io update

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
+import queryString from "query-string"
+import io from "socket.io-client"
 
-export class Chat extends Component {
-    render() {
-        return (
-            <h1>
-                Chat
-            </h1>
-        )
-    }
+const Chat = ({ location }) => {
+
+    const [name, setName] = useState('')
+    const [room, setRoom] = useState('')
+    const ENDPOINT = "127.0.0.1:5000"
+    
+
+    useEffect(() => {
+        const {name, room} = queryString.parse(location.search)
+        const socket = io(ENDPOINT)
+
+        setName(name)
+        setRoom(room)
+
+        // console.log(socket)
+        socket.emit("join", {name, room})
+
+        
+    }, ENDPOINT, location.search)
+
+    return (
+        <h1>
+            Chat
+        </h1>
+    )
 }
 
 export default Chat
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const socketio = require('socket.io')
 const http = require("http")
+var cors = require('cors')
 
 
 // DATA
@@ -13,11 +14,17 @@ const io = socketio(server)
 
 io.on("connection", (socket) => {
     console.log("User jadid mowa7e!!");
+    socket.on("join", ({name, room}) => {
+        console.log(`${name} => ${room}`)
+       // console.log(data)
+    })
+    
     socket.on("disconnect", () => {
         console.log("mexa 3and lah lil2asaf")
     })
 })
 
+app.use(cors())
 app.use(router)
 
 server.listen(PORT, console.log(`Listening to port ${PORT}`))
